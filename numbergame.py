f76d08a510f20b74560f4989cfbd3bdb035b55a3
AveryBau <averya34@nycstudents.net>
2026-02-09 10:17:17 -0500

    Create numbergame.py

---
+++ numbergame.py
@@ -0,0 +1,15 @@
+history = []
+guess = int(input("guess a number 1-10"))
+import random
+print(random.randint(1,10))
+def random(guess):
+    global_guess = 0
+    for i in range(1,10):
+        if guess == random:
+            print("correct")
+        else:
+            history.append(guess)
+            print(guess)
+
+
+    

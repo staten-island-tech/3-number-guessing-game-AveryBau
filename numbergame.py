ca26b6278bc8cac1df42910287f9f9bab192613f
AveryBau <averya34@nycstudents.net>
2026-02-10 09:44:22 -0500

    Update numbergame.py

--- numbergame.py
+++ numbergame.py
@@ -1,15 +1,25 @@
-history = []
+""" history = []
 guess = int(input("guess a number 1-10"))
 import random
 print(random.randint(1,10))
-def random(guess):
+def we(guess):
     global_guess = 0
     for i in range(1,10):
-        if guess == random:
+        if guess == int(random):
             print("correct")
-        else:
-            history.append(guess)
-            print(guess)
+            
+        elif guess < random:
+            history.append(i)
+            print("higher" and history)
+            
+        elif guess > random:
+            history.append(i)
+            print("lower" and history)
+            
+we(guess) """
 
 
-    
+i = 1
+while i < 6:
+    print(i)
+    i += 1
